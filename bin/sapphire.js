e75a43ec953774682ae9f261096a5e8b735654dd
Fadion Dashi <jonidashi@gmail.com>
2018-03-07 18:45:19 +0100

    Add custom logger for better console color output
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- bin/sapphire.js
+++ bin/sapphire.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const program = require('caporal');
 const version = require('../package.json').version
-
 const GenerateController = require('../lib/commands/generate-controller')
 
 program
