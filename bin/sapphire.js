8ec322fc8cce0f8e513afa287ae35829f0c8fcce
Fadion Dashi <jonidashi@gmail.com>
2018-03-08 13:46:18 +0100

    Add gen:middleware command
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- bin/sapphire.js
+++ bin/sapphire.js
@@ -2,6 +2,7 @@
 const program = require('caporal');
 const version = require('../package.json').version
 const GenerateController = require('../lib/commands/generate-controller')
+const GenerateMiddleware = require('../lib/commands/generate-middleware')
 
 program
   .version(version)
@@ -13,5 +14,10 @@ program
   .option('--methods <methods>', 'Controller methods', program.LIST)
   .option('--force', 'Overwrite if file exists')
   .action(new GenerateController().action)
+
+  .command('gen:middleware', 'Generate a Middleware').alias('g:mw')
+  .argument('<name>', 'Middleware class name')
+  .option('--force', 'Overwrite if file exists')
+  .action(new GenerateMiddleware().action)
 
 program.parse(process.argv)
